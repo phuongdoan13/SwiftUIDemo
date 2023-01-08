127e829018b40b1d9712905f1701ee7a2e47c08b
phuongdoan13 <mphuongdoant@gmail.com>
2023-01-09 09:44:27 +1100

    Finish up creating dynamic list

---
+++ SwiftUI Demo/Views/CircleImage.swift
@@ -0,0 +1,26 @@
+//
+//  CircleImage.swift
+//  SwiftUI Demo
+//
+//  Created by Harry on 11/1/2023.
+//
+
+import SwiftUI
+
+struct CircleImage: View {
+    var image: Image
+    var body: some View {
+        image
+            .clipShape(Circle())
+            .overlay{
+                Circle().stroke(.white, lineWidth: 4)
+            }
+            .shadow(radius: 7)
+    }
+}
+
+struct CircleImage_Previews: PreviewProvider {
+    static var previews: some View {
+        CircleImage(image: Image("turtlerock"))
+    }
+}
