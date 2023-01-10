8fcfe529cc44e1c68495fce6b2e4eb2e01b72b08
phuongdoan13 <mphuongdoant@gmail.com>
2023-01-10 21:21:05 +1100

    Initial Commit

---
+++ SwiftUI Demo/SwiftUI_DemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_DemoApp.swift
+//  SwiftUI Demo
+//
+//  Created by Harry on 10/1/2023.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_DemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
