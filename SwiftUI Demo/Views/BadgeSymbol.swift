bf48f086c367040c2c4a8eabfe5473dd4b8d79cf
phuongdoan13 <mphuongdoant@gmail.com>
2023-02-18 17:40:14 +1100

    Added files

---
+++ SwiftUI Demo/Views/BadgeSymbol.swift
@@ -0,0 +1,20 @@
+//
+//  BadgeSymbol.swift
+//  SwiftUI Demo
+//
+//  Created by Harry on 15/4/2023.
+//
+
+import SwiftUI
+
+struct BadgeSymbol: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct BadgeSymbol_Previews: PreviewProvider {
+    static var previews: some View {
+        BadgeSymbol()
+    }
+}
